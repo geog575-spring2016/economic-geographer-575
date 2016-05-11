2e8a1f3c391a5d24a82226f430f08bba6016311d
catherinestreiffer <catherinestreiffer@gmail.com>
2016-05-10 20:43:58 -0500

    Switched to Leaflet Map

--- js/buildChoropleth.js
+++ js/buildChoropleth.js
@@ -1,47 +1,53 @@
 var mouseMoveControl = true;
 function initializeChoropleth(data) {
-  var map = d3.select("#map2")
-    .append("svg")
-    .attr("id", "d3map")
-    .attr("class", "map")
-    .attr("width", "100%")
-    .attr("height", "100%");
-    var projection = d3.geo.albers()
-      .center([-80, 40])
-      .rotate([90, 0, 0])
-      .parallels([30.5, 44.8])
-      .scale(1000)
-      .translate([0, 0]);
-      var path = d3.geo.path()
-        .projection(projection);
-  setEnumerationUnits(map, path, data)
+  var map = L.map('map2' ,{
+      center: [40, -120],
+      zoom: 4
+  })
+
+  createTileLayer(map)
+
+  var dataLayer = setEnumerationUnits(map, dataLayer, data)
   return {
     map:map,
-    projection:projection,
-    path:path,
+    dataLayer: dataLayer
+//    projection:projection,
+//    path:path,
   }
 
 }
-
-function setEnumerationUnits(map, path, data){
-var counties = map.selectAll()
-    .data(data.features)
-    .enter()
-    .append("path")
-    .attr("class","counties")
-    .attr("fips", function(d){
-        return d.properties.FIPS;
-    })
-    .attr("d", path)
+function createTileLayer(map) {
+  var mapboxAccessToken = "pk.eyJ1IjoiY2F0aGVyaW5lc3RyZWlmZmVyIiwiYSI6ImNpa3BrOTVlaTEyNmZ0aWo3eDlyaThraGMifQ.bYUxm5s4cRD2iRGqvpNNBA"
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    attribution: '&copy; <a href="http://mapbox.com/">Mapbox</a> icon: Invitro Estudio; data source: UNHCR'
+}).addTo(map);
+}
+function setEnumerationUnits(map, dataLayer, data) {
+  var dataLayer = L.geoJson(data, {className: "counties"}).addTo(map)
+  dataLayer.setStyle(function(feature) {
+    return {
+      weight: .1,
+      color: 'black',
+      dashArray: '3',
+      opacity: 1,
+      fillOpacity: 1,
+    }
+  })
+//var counties = map.selectAll()
+//    .attr("class","counties")
+//    .attr("fips", function(d){
+//        return d.properties.FIPS;
+//    })
+//    .attr("d", path)
 //    .style("fill", function(d){
 //          return choropleth(d.properties, bivariates);
 //    })
-    .style("stroke", "#000")
-    .style("stroke-width", "0.1px")
 
   //add style descriptor to each path
-var desc = counties.append("desc")
-    .text('{"stroke": "#000", "stroke-width": "0.5px"}');
+//var desc = counties.append("desc")
+//    .text('{"stroke": "#000", "stroke-width": "0.5px"}');
+return dataLayer
 };
 
 function updateChoropleth(choropleth, FIPSxData, xCol, FIPSyData, yCol, year, bivariate) {
@@ -55,15 +61,13 @@ function updateChoropleth(choropleth, FIPSxData, xCol, FIPSyData, yCol, year, bi
   colors[[-1,-1]]=window.getComputedStyle(document.getElementById("A3")).fill
   colors[[ 0,-1]]=window.getComputedStyle(document.getElementById("B3")).fill
   colors[[ 1,-1]]=window.getComputedStyle(document.getElementById("C3")).fill
-  var counties = d3.selectAll(".counties")
-      .transition()
-      .duration(1000)
-      .style('fill',function(d){
-        xVal = FIPSxData[d.properties.FIPS]
-        yVal = FIPSyData[d.properties.FIPS]
-        lookupXCoord = -1 + (xVal>=xQuantile01) + (xVal>=xQuantile02)
-        lookupYCoord = -1 + (yVal>=yQuantile01) + (yVal>=yQuantile02)
-        return colors[[lookupXCoord,lookupYCoord]]
-//          return choropleth(d.propertiesFIPS], bivariates)
-      });
+  choropleth.dataLayer.eachLayer(function(layer) {
+    xVal = FIPSxData[layer.feature.properties.FIPS]
+    yVal = FIPSyData[layer.feature.properties.FIPS]
+    lookupXCoord = -1 + (xVal>=xQuantile01) + (xVal>=xQuantile02)
+    lookupYCoord = -1 + (yVal>=yQuantile01) + (yVal>=yQuantile02)
+    layer.setStyle({
+                    fillColor: colors[[lookupXCoord,lookupYCoord]]
+                });
+  })
 }
